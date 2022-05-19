7e4ee8b5f0e4f4efb08d998f488b6e4f13fbb013
Itsyourboigenix <104576794+Itsyourboigenix@users.noreply.github.com>
2022-05-19 19:51:30 +0530

    Update and rename Main Code to Main Code.js

---
+++ Main Code.js
@@ -0,0 +1,16 @@
+Go To Chrome://dino
+
+Right Click > Inspect > Console > Enter Code
+
+This is Javascript Language
+
+
+
+
+Runner.instance_.setSpeed(1000)
+
+Runner.prototype.gameOver = function(){}
+
+Runner.instance_.distanceRan=100000/
+
+Runner.instance_.distanceMeter.config.COEFFICIEN
